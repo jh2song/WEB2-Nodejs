0c7bf98a3a84cc840dbf84cda43a87975f1085ac
jh2song <spec0003@naver.com>
2021-03-31 14:05:35 +0900

    Add variable2.js

---
+++ syntax/variable2.js
@@ -0,0 +1,3 @@
+var name = 'Jihoon';
+var letter = 'Dear ' + name + ' fsdkajahsflsjlhsajd ' + name + ' sjasjdhla s hdjahfsadhjfds' + name + ' sjdhjlasdhfa dh dsajh ' + name + ' dsjslad lasd lsdafhj';
+console.log(letter);
