d19b60e9eb4155da2f105486797fa4ce3927d887
jh2song <spec0003@naver.com>
2021-04-07 07:02:10 +0900

    Add comparison.js

---
+++ syntax/comparison.js
@@ -0,0 +1,8 @@
+console.log(1==1); // true
+console.log(1==2); // false
+console.log(1>2); // false
+console.log(1<2); // true
+
+// 정말 같은가?
+console.log(1===1); // true
+console.log(1===2); // false
