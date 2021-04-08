18d44ff0f970e6e347046beaeec8eda345864daf
jh2song <spec0003@naver.com>
2021-04-08 09:11:48 +0900

    Getting arguments practice

--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,6 +1,14 @@
+var args = process.argv;
+console.log(args);
+/*
+출력
+[ 'C:\\Program Files\\nodejs\\node.exe',
+  'C:\\Users\\spec0\\Desktop\\Project\\2021\\03\\nodejs\\syntax\\conditional.js' ]
+*/
+
 console.log('A');
 console.log('B');
-if (true) {
+if (args[2] === '1') { // idx 2부터 파라미터가 들어온다
     console.log('C1');
 } else {
     console.log('C2');
