62253285ba5952bb5b0460bbb08be0b1269c185f
jh2song <spec0003@naver.com>
2021-04-11 02:34:04 +0900

    Created function.js

---
+++ syntax/function.js
@@ -0,0 +1,13 @@
+f123();
+console.log("Test1");
+f123();
+console.log("Test2");
+f123();
+console.log("Test3");
+
+function f123() {
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+}
