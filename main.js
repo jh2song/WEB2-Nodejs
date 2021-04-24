b54fd0e4fc98b2fd4acc8c7e8462ffe6926ee13f
jh2song <spec0003@naver.com>
2021-04-24 09:17:10 +0900

    Post receiving implemented

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body) {
     return `
@@ -62,7 +63,7 @@ var app = http.createServer(function(request, response) {
             var title = 'WEB - create';
             var list = templateList(filelist);
             var template = templateHTML(title, list, `
-            <form action="http://localhost:3000/process_create" method="post">
+            <form action="http://localhost:3000/create_process" method="post">
             <p><input type="text" name="title" placeholder="title"></p>
             <p>
                 <textarea name="description" placeholder="description"></textarea>
@@ -75,6 +76,19 @@ var app = http.createServer(function(request, response) {
             response.writeHead(200);
             response.end(template); 
         });
+    } else if (pathname === '/create_process') { // post로 값 받기
+        var body = '';
+        request.on('data', function(data) {
+            body = body + data;
+        });
+        request.on('end', function() {
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+            console.log(post); // { title: 'nodejs', description: 'nodejs is ...' } 출력
+        });
+        response.writeHead(200);
+        response.end('success'); 
     } else {
         response.writeHead(404);
         response.end('Not found'); 
