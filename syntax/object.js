ec0c3f999fa974a11faf34ff6f58bc97640008fb
jh2song <spec0003@naver.com>
2021-04-27 08:15:19 +0900

    Created object.js

---
+++ syntax/object.js
@@ -0,0 +1,11 @@
+// Array
+var members = ['egoing', 'k8805', 'hoya'];
+console.log(members[1]); // k8805
+
+// Object
+var roles = {
+    'programmer':'egoing',
+    'designer':'k8805',
+    'manager':'hoya'
+}
+console.log(roles.designer); // k8805
