43efe3801b8b7c70dafaf6fbcab2cd8410b06d4b
jh2song <spec0003@naver.com>
2021-04-27 08:44:00 +0900

    Created object3.js

---
+++ syntax/object3.js
@@ -0,0 +1,18 @@
+var o = {
+    v1:'v1',
+    v2:'v2',
+    f1:function () {
+        console.log(this.v1);
+    },
+    f2:function () {
+        console.log(this.v2);
+    }
+}
+
+/*
+출력
+v1
+v2
+*/
+o.f1();
+o.f2();
