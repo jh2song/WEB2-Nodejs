ee1e87592148de6bc9bfecba54eabfd8d619d943
jh2song <spec0003@naver.com>
2021-04-27 08:21:19 +0900

    Loop in object learned

--- syntax/object.js
+++ syntax/object.js
@@ -2,6 +2,17 @@
 var members = ['egoing', 'k8805', 'hoya'];
 console.log(members[1]); // k8805
 
+/*
+array loop egoing
+array loop k8805
+array loop hoya
+*/
+var i = 0;
+while(i < members.length) {
+    console.log('array loop', members[i]);
+    i = i + 1;
+}
+
 // Object
 var roles = {
     'programmer':'egoing',
@@ -9,3 +20,13 @@ var roles = {
     'manager':'hoya'
 }
 console.log(roles.designer); // k8805
+console.log(roles['designer']); // k8805
+
+/*
+object =>  programmer value =>  egoing
+object =>  designer value =>  k8805
+object =>  manager value =>  hoya
+*/
+for (var name in roles) {
+    console.log('object => ', name, 'value => ', roles[name]);
+}
